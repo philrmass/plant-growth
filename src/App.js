ca66a7520a5b2b2509ee93ea917d8ef078f5d94e
Phil Mass <philrmass@gmail.com>
2017-09-03 22:12:20 -0700

    Adding files updated from create-react-app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>A Plant Growth L-System</h2>
+        </div>
+        <div className="App-border">
+        </div>
+        <div className="App-body">
+        <p>
+          PLACE PLANTS HERE
+        </p>
+        </div>
+        <div className="App-border">
+        </div>
+        <div className="App-footer">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
